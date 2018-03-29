a136f56fad394753690e3aa2a9d3c0d148fe9a8c
Zach <ssb.pub.key.is@ZqH7Mctu/7DNInxuwl12ECjfrAKUX2tBLq1rOldNhg0=.ed25519>
2018-03-29 20:26:58 +1300

    added server credentials

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express') // Use express for server
+var hbs = require('express-handlebars') //Use handlebars for our templating
+var bodyParser = require('body-parser') //?? I think it's used for handlebar
+
+var server = express()
+
+// Middleware-- don't fully know what this means.
+server.engine('hbs', hbs({
+  defaultLayout: 'main',
+  extname: 'hbs'
+}))
+
+server.set('view engine', 'hbs')
+server.use(express.static('public'))
+server.use(bodyParser.urlencoded({ extended: false }))
+
+server.get('/', function (req,res) {
+  res.send('hello world!')
+})
+
+module.exports = server
+
